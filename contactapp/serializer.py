cc726e1c46dbcd551a5962ed6ce76f49cf3bdc07
vnay-krshn <vinaykrishnanh.inbox@gmail.com>
2020-10-08 13:15:35 +0530

    Second commit html serializer view

---
+++ contactapp/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from contactapp.models import Contacts
+
+class ContactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contacts
+        fields = ('id', 'name', 'email', 'phone', 'description')
--- contactapp/views.py
+++ contactapp/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-from .models import Contacts
+from django.http.response import JsonResponse
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+
+from contactapp.models import Contacts
+from contactapp.serializer import ContactSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
 def index(request):
     return render(request, 'contactapp/index.html')
+
+@api_view(['GET','POST'])
+
+def contactDetails(request):
+    if request.method == 'GET'
+        contacts = Contacts.objects.all()
+        name = request.GE.get('name', None)
+        if name is not None:
+            contacts = contacts.filter(name_icontains = name)
+        contactSerial = ContactSerializer(contacts, many= True)
+        return JsonResponse(contactSerial.data, safe = False)
+    
+    elif request.method == 'POST':
+        contactData = JSONParser().parse(request)
+        contactSerial = ContactSerializer(data = contactData)
+        if contactSerial.is_valid():
+            contactSerial.save()
+            return JsonResponse(contactSerial.data, status = status.HTTP_201_CREATED)
+         return JsonResponse(contactSerial.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+
 
 
