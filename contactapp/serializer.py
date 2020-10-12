b54a336ccc00fd62f68ddb475cc73eac914f1524
vnay-krshn <vinaykrishnanh.inbox@gmail.com>
2020-10-12 11:50:06 +0530

    changes 4.0

--- contactapp/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from contactapp.models import Contacts
-
-class ContactSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Contacts
-        fields = ('id', 'name', 'email', 'phone', 'description')
